0c323754ea6e9266c27d1230fb1582661a0be05e
prathap-theguru <55411056+prathap-theguru@users.noreply.github.com>
2022-04-30 23:04:03 -0400

    Delete schedule_pgcron_job.sh

--- scripts/schedule_pgcron_job.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-export PGPASSWORD=$RPTDBPASSWORD
-psql -h${RPTHOST} -p ${RPTDBPORT} -U${RPTDBUSER} -d ${RPTDB} <<EOF
-    delete from public.employee;
-    call schedule_sp_job()
-EOF
-
